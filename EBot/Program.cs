131fde1144b0f8e2d2b35f255b82e53359d84206
Earu <reno.reboursier@gmail.com>
2017-11-17 10:19:41 +0100

    update lua sandbox

--- EBot/Program.cs
+++ EBot/Program.cs
@@ -13,7 +13,7 @@ namespace EBot
         {
             await EBotCredentials.Load();
 
-            EBotClient client = new EBotClient(EBotCredentials.TOKEN_DEV, "$^");
+            EBotClient client = new EBotClient(EBotCredentials.TOKEN_DEV, "_");
 
             await client.ConnectAsync();
             await Task.Delay(-1);
