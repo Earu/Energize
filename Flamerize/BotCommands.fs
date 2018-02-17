2abc4f45c0aa6b3593d63829d09a829c54fb0505
Earu <reno.reboursier@gmail.com>
2018-02-18 00:46:09 +0100

    add flamerize
    - Added the source for "flamerize" a more personal bot to manage my discord server, with more personal features

---
+++ Flamerize/BotCommands.fs
@@ -0,0 +1,36 @@
+﻿namespace Flamerize
+
+open System.Threading.Tasks
+open DSharpPlus.CommandsNext
+open DSharpPlus.CommandsNext.Attributes
+open Utils
+open DSharpPlus.Entities
+open HtmlAgilityPack
+
+type BotCommands() =
+
+    [<Command("ping"); Description("Responds with socket latency.")>]
+    member public self.PingAsync(ctx: CommandContext) = 
+        async {
+            do! ctx.TriggerTypingAsync() |> Async.AwaitTask
+            do! ctx.RespondAsync(sprintf "Socket latency: %ims" ctx.Client.Ping) |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
+    
+    [<Command("glua"); Description("Gets info on a glua function")>]
+    member public self.GluaAsync(ctx: CommandContext) ([<Description("The function to look for")>] ``text``: string) = 
+        async {
+            do! ctx.TriggerTypingAsync() |> Async.AwaitTask
+            let endpoint = "https://samuelmaddock.github.io/glua-docs/#?q=" + ``text``
+        
+            match HTTP.Fetch endpoint with
+            | Some(content) -> 
+                let doc = new HtmlDocument()
+                doc.LoadHtml(content)
+                let node = doc.DocumentNode.SelectSingleNode("//article")
+                let gwiki = node.SelectSingleNode("//a[@class]").InnerText
+                //let github = node.SelectSingleNode("//a[@class='extra']/@href").InnerText
+                //let func = node.SelectSingleNode("//div[@class='function_line']").InnerText
+
+                do! ctx.RespondAsync(sprintf "%s" gwiki) |> Async.AwaitTask |> Async.Ignore
+            | None -> do! ctx.RespondAsync("not found") |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
---
+++ Flamerize/Utils.fs
@@ -0,0 +1,31 @@
+﻿namespace Flamerize
+
+open System
+open System.Text
+open System.Net
+open System.IO
+
+module Utils =
+    [<AbstractClass; Sealed>]
+    type public HTTP() =
+        static member UserAgent = "Flamerize Discord(Earu's Bot)"
+        static member Fetch (url: String): Option<string> =
+            try
+                let req: HttpWebRequest = WebRequest.Create url :?> HttpWebRequest
+                req.Method <- "GET"
+                req.Timeout <- 1000 * 60
+                req.Headers.[HttpRequestHeader.UserAgent] <- HTTP.UserAgent
+
+                let content = Async.RunSynchronously <| async{
+                    let! answer = Async.AwaitTask (req.GetResponseAsync())
+                    let stream = answer.GetResponseStream()
+                    let reader = new StreamReader(stream,Encoding.UTF8)
+                    let result = reader.ReadToEnd()
+
+                    return result
+                }
+
+                Some(content)
+
+            with
+            | _ -> None
