101eba7736274a84089dd95ae980a16872c6047e
Earu <earu.derg@gmail.com>
2018-02-18 12:02:47 +0100

    More work on Flamerize
    Proper naming of filesm types, and modules, add MessageSender

---
+++ Flamerize/Commands.fs
@@ -0,0 +1,39 @@
+﻿namespace Flamerize
+
+open System.Threading.Tasks
+open DSharpPlus.CommandsNext
+open DSharpPlus.CommandsNext.Attributes
+open Utils
+open DSharpPlus.Entities
+open HtmlAgilityPack
+
+type Commands() =
+
+    [<Command("ping"); Description("Responds with socket latency.")>]
+    member public self.PingAsync(ctx: CommandContext) = 
+        async {
+            do! ctx.TriggerTypingAsync() |> Async.AwaitTask
+            do! ctx.RespondAsync(sprintf "Socket latency: %ims" ctx.Client.Ping) |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
+    
+    [<Command("glua"); Description("Gets info on a glua function")>]
+    member public self.GluaAsync(ctx: CommandContext) ([<Description("The function to look for")>] ``text``: string) = 
+        async {
+            do! ctx.TriggerTypingAsync() |> Async.AwaitTask
+            let parts = ``text``.Split '.'
+            let query = match parts.Length > 1 with
+                        | true -> ``text``.Replace('.','/')
+                        | false -> "Global/" + ``text``
+
+            let endpoint = "http://wiki.garrysmod.com/page/" + query
+        
+            match HTTP.Fetch endpoint with
+            | Some(content) -> 
+                let doc = new HtmlDocument()
+                doc.LoadHtml content
+                //let node = doc.DocumentNode.SelectSingleNode ""
+
+                let embed = MessageSender.CreateEmbed ctx.User "GLua" endpoint
+                do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
+            | None -> do! ctx.RespondAsync("Coulnd't find this function") |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
---
+++ Flamerize/MessageSender.fs
@@ -0,0 +1,17 @@
+﻿namespace Flamerize
+
+open DSharpPlus.Entities
+
+[<AbstractClass; Sealed>]
+type public MessageSender() =
+    
+    static member CreateEmbed (user: DiscordUser) (head: string) (txt: string): DiscordEmbed = 
+        let builder = new DiscordEmbedBuilder()
+        builder.Author <- new DiscordEmbedBuilder.EmbedAuthor()
+        builder.Author.Name <- user.Username
+        builder.Author.IconUrl <- user.AvatarUrl
+        builder.Color <- new DiscordColor(0xdddddd)
+        builder.Footer <- new DiscordEmbedBuilder.EmbedFooter()
+        builder.Footer.Text <- head
+        builder.Description <- txt
+        builder.Build()
