b3fd7de1ce95a511afe84240a4613ea5d292c382
R.Réno <earu.derg@gmail.com>
2018-02-22 00:48:37 +0100

    Flamerize: 2 new commands
    - Added "colors" command to see available colors that you can set for yourself
    
    - Added "color" to set your color
    
    - Added helper methods in Commands type

--- Flamerize/Commands.fs
+++ Flamerize/Commands.fs
@@ -6,8 +6,21 @@ open DSharpPlus.CommandsNext
 open DSharpPlus.CommandsNext.Attributes
 open Utils
 open HtmlAgilityPack
+open System.Drawing
+open System.Collections
+open System
+open DSharpPlus.Entities
+open DSharpPlus
+
+
 
 type Commands() =
+    member private self.Colors = [
+        "Teal",Color.Teal; "Green",Color.Green; "LightGreen",Color.LightGreen; "DarkGreen",Color.DarkGreen; "Blue",Color.Blue;
+        "DarkBlue",Color.DarkBlue; "Cyan",Color.Cyan; "DarkCyan",Color.DarkCyan; "Purple",Color.Purple; "Magenta",Color.Magenta
+        "DarkMagenta",Color.DarkMagenta; "Gold",Color.Gold; "DarkGold",Color.DarkGoldenrod; "Orange",Color.Orange; "DarkOrange",Color.DarkOrange
+        "Red",Color.Red; "DarkRed",Color.DarkRed; "Pink",Color.Pink; "Fuchsia",Color.Fuchsia; "Yellow",Color.Yellow; "Black",Color.Black; "White",Color.White
+    ]
 
     [<Command("ping"); Description("Responds with socket latency.")>]
     member public self.PingAsync(ctx: CommandContext) = 
@@ -45,28 +58,100 @@ type Commands() =
                     let call = match Seq.length argnames > 0 with
                                 | true -> argnames |> String.concat ", " 
                                 | false -> ""
-                
-                    let aseqdisplay = seq { for x in 0..((match args with | null -> 0 | _ -> Seq.length args) - 1) -> sprintf "%s" (Seq.item  x argdescs) }
-                    let mutable argdisplay = match Seq.length aseqdisplay > 0 with
-                                                | true -> aseqdisplay |> String.concat ", " 
-                                                | false -> "None"
-                    argdisplay <- Regex.Replace(argdisplay,"\.","")
-                    argdisplay <- Regex.Replace(argdisplay,"\s{3,}","\n")
-                    argdisplay <- Regex.Replace(argdisplay,"\s+"," ").TrimStart()
+                    
+                    //Returns the current seq passed or an empty one
+                    let seq_or_default list = 
+                        match list with
+                        | null -> Seq.empty
+                        | _ -> list
+
+                    //Func to have max amount of values in a list
+                    let max list = Seq.length (seq_or_default list) - 1
+                    
+                    //For the string display in embed
+                    let display list = 
+                        let mutable disp = match Seq.length (seq_or_default list) > 0 with
+                                            | true -> list |> String.concat ", " 
+                                            | false -> "None"
+                        disp <- Regex.Replace(disp,"\.","")
+                        disp <- Regex.Replace(disp,"\s{3,}","\n")
+                        Regex.Replace(disp,"\s+"," ").TrimStart()
+                        
+                    let argseq = seq { for x in 0..max args -> sprintf "%s" (Seq.item  x argdescs) }
+                    let argdisplay = display argseq
 
                     let rets = root.SelectNodes "//div[@class='return']"
                     let retdescs = seq { for ret in rets -> ret.InnerText }
-                    let rseqdisplay = seq { for x in 0..((match rets with | null -> 0 | _ -> Seq.length rets) - 1) -> sprintf "%s" (Seq.item x retdescs)}
-                    let mutable retdisplay = match Seq.length rseqdisplay > 0 with
-                                                | true -> rseqdisplay |> String.concat ", "
-                                                | false -> "None"
-                    retdisplay <- Regex.Replace(retdisplay,"\.","")
-                    retdisplay <- Regex.Replace(retdisplay,"\s{3,}","\n")
-                    retdisplay <- Regex.Replace(retdisplay,"\s+"," ").TrimStart()
+                    let retseq = seq { for x in 0..max rets -> sprintf "%s" (Seq.item x retdescs)}
+                    let retdisplay = display retseq
                 
-                    let desc = sprintf "**CALL:** %s(%s)\n**REALM:** %s\n**ARGS:**\n%s\n**RETURNS:**\n%s\n**WIKI:** %s" ``text`` call realm argdisplay retdisplay endpoint
+                    let mutable desc = sprintf "**CALL:** %s(%s)\n**REALM:** %s\n**ARGS:**\n%s\n" ``text`` call realm argdisplay
+                    desc <- desc + sprintf "**RETURNS:**\n%s\n**WIKI:** %s" retdisplay endpoint
                     MessageSender.CreateEmbed ctx.User "GLua" desc EmbedColor.Good                
                 | None -> MessageSender.CreateEmbed ctx.User "GLua" "Couln't find this function" EmbedColor.Warning
             
             do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
+    
+    [<Command("colors"); Description("See the colors available")>]
+    member public self.ColorsAsync(ctx: CommandContext) =
+        async{
+            let colors = seq { for (k,_) in self.Colors -> k } |> String.concat ", "
+            let desc = sprintf "```\n%s```" colors
+            let embed = MessageSender.CreateEmbed ctx.User "Colors" desc EmbedColor.Good
+            do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
+        } |> Async.StartAsTask :> Task
+    
+    member private self.CToFList<'T> clist = seq { for (x: 'T) in clist -> x } |> Seq.toList
+
+    member private self.GetRole(_member: DiscordMember) (name: string): Option<DiscordRole> =
+        let predicate (x: DiscordRole) = x.Name.Equals(name)
+        let roles = self.CToFList _member.Roles
+        match List.exists predicate roles with
+        | true -> Some(List.find predicate roles)
+        | false -> None
+    
+    member private self.GetColorRoles(_member: DiscordMember): List<DiscordRole> =
+        let mutable roles = List.empty
+        for (name: string,_: Color) in self.Colors do
+            let opt = self.GetRole _member name
+            if opt.IsSome then
+                roles <- opt.Value :: roles
+        roles
+
+    [<Command("color"); Description("Sets your color")>]
+    member public self.ColorAsync(ctx: CommandContext) ([<Description("The color to apply")>]``color``: string) =
+        let prediccol (x: string,_: Color) = x.Equals(``color``) 
+        let colorfound = List.exists prediccol self.Colors
+        async{
+            match colorfound with
+            | true -> 
+                let predicrole (role: DiscordRole) = role.Name.Equals(``color``)
+                let roles = self.CToFList ctx.Guild.Roles
+                let mutable role = if List.exists predicrole roles then List.find predicrole roles else null
+                role <- match role with
+                        | null -> 
+                            let (_,c) = List.find prediccol self.Colors
+                            let col = new Nullable<DiscordColor>(new DiscordColor(c.R,c.G,c.B))
+                            let newr = ctx.Guild.CreateRoleAsync(``color``,Nullable(),col) |> Async.AwaitTask |> Async.RunSynchronously
+                            let botmember = ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id) |> Async.AwaitTask |> Async.RunSynchronously
+                            let mutable highest = 0
+                            for r in botmember.Roles do
+                                highest <- if r.Position > highest then r.Position else highest
+                            ctx.Guild.UpdateRolePositionAsync(newr,highest - 1) |> Async.AwaitTask |> Async.RunSynchronously
+                            newr
+                        | _ -> role
+                
+                let! _member = ctx.Guild.GetMemberAsync(ctx.User.Id) |> Async.AwaitTask
+                for r in self.GetColorRoles _member do
+                    do! ctx.Guild.RevokeRoleAsync(_member,r,"Color change") |> Async.AwaitTask |> Async.Ignore
+                
+                do! ctx.Guild.GrantRoleAsync(_member,role) |> Async.AwaitTask |> Async.Ignore
+                let embed = MessageSender.CreateEmbed ctx.User "Color" (sprintf "Color successfully set to %s" ``color``) EmbedColor.Good
+                do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
+            | false ->
+                let colors = seq { for (k,_) in self.Colors -> k } |> String.concat ", "
+                let desc = sprintf "Couln't find a color corresponding to your input!\nAvailable colors:\n```\n%s```" colors
+                let embed = MessageSender.CreateEmbed ctx.User "Color" desc EmbedColor.Warning
+                do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
         } |> Async.StartAsTask :> Task
