c8e5dfff3c22765daeaeef9756c5e2ecd499ac8c
R.Réno <reno.reboursier@gmail.com>
2018-02-21 15:06:33 +0100

    Update flamerize
    - Added functional glua command for info about gmod lua api
    
    - Modified MessageSender to take a color enum of my own

--- Flamerize/Commands.fs
+++ Flamerize/Commands.fs
@@ -1,10 +1,10 @@
 ﻿namespace Flamerize
 
+open System.Text.RegularExpressions
 open System.Threading.Tasks
 open DSharpPlus.CommandsNext
 open DSharpPlus.CommandsNext.Attributes
 open Utils
-open DSharpPlus.Entities
 open HtmlAgilityPack
 
 type Commands() =
@@ -13,7 +13,8 @@ type Commands() =
     member public self.PingAsync(ctx: CommandContext) = 
         async {
             do! ctx.TriggerTypingAsync() |> Async.AwaitTask
-            do! ctx.RespondAsync(sprintf "Socket latency: %ims" ctx.Client.Ping) |> Async.AwaitTask |> Async.Ignore
+            let embed = MessageSender.CreateEmbed ctx.User "Ping" (sprintf "Socket latency: %ims" ctx.Client.Ping) EmbedColor.Good
+            do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
         } |> Async.StartAsTask :> Task
     
     [<Command("glua"); Description("Gets info on a glua function")>]
@@ -22,18 +23,50 @@ type Commands() =
             do! ctx.TriggerTypingAsync() |> Async.AwaitTask
             let parts = ``text``.Split '.'
             let query = match parts.Length > 1 with
-                        | true -> ``text``.Replace('.','/')
+                        | true -> Regex.Replace(``text``,"(\.|:)","/")
                         | false -> "Global/" + ``text``
 
             let endpoint = "http://wiki.garrysmod.com/page/" + query
-        
-            match HTTP.Fetch endpoint with
-            | Some(content) -> 
-                let doc = new HtmlDocument()
-                doc.LoadHtml content
-                //let node = doc.DocumentNode.SelectSingleNode ""
+            let embed = 
+                match HTTP.Fetch endpoint with
+                | Some(content) -> 
+                    let doc = new HtmlDocument()
+                    doc.LoadHtml content
+                    let root = doc.DocumentNode
+                    let realm = (root.SelectSingleNode "//*[@id='bodyContent']/div[4]/div[1]/span").GetAttributeValue("title","NOT_FOUND")
+                
+                    let args = root.SelectNodes "//div[@class='argument']"
+                    let argchunks = root.SelectNodes "//span[@class='arg_chunk']"
+                    let argnames = match argchunks with 
+                                    | null -> Seq.empty 
+                                    | _ -> seq { for arg in argchunks -> arg.InnerText }
+                
+                    let argdescs = seq { for arg in args -> arg.InnerText }
+                    let call = match Seq.length argnames > 0 with
+                                | true -> argnames |> String.concat ", " 
+                                | false -> ""
+                
+                    let aseqdisplay = seq { for x in 0..((match args with | null -> 0 | _ -> Seq.length args) - 1) -> sprintf "%s" (Seq.item  x argdescs) }
+                    let mutable argdisplay = match Seq.length aseqdisplay > 0 with
+                                                | true -> aseqdisplay |> String.concat ", " 
+                                                | false -> "None"
+                    argdisplay <- Regex.Replace(argdisplay,"\.","")
+                    argdisplay <- Regex.Replace(argdisplay,"\s{3,}","\n")
+                    argdisplay <- Regex.Replace(argdisplay,"\s+"," ").TrimStart()
 
-                let embed = MessageSender.CreateEmbed ctx.User "GLua" endpoint
-                do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
-            | None -> do! ctx.RespondAsync("Coulnd't find this function") |> Async.AwaitTask |> Async.Ignore
+                    let rets = root.SelectNodes "//div[@class='return']"
+                    let retdescs = seq { for ret in rets -> ret.InnerText }
+                    let rseqdisplay = seq { for x in 0..((match rets with | null -> 0 | _ -> Seq.length rets) - 1) -> sprintf "%s" (Seq.item x retdescs)}
+                    let mutable retdisplay = match Seq.length rseqdisplay > 0 with
+                                                | true -> rseqdisplay |> String.concat ", "
+                                                | false -> "None"
+                    retdisplay <- Regex.Replace(retdisplay,"\.","")
+                    retdisplay <- Regex.Replace(retdisplay,"\s{3,}","\n")
+                    retdisplay <- Regex.Replace(retdisplay,"\s+"," ").TrimStart()
+                
+                    let desc = sprintf "**CALL:** %s(%s)\n**REALM:** %s\n**ARGS:**\n%s\n**RETURNS:**\n%s\n**WIKI:** %s" ``text`` call realm argdisplay retdisplay endpoint
+                    MessageSender.CreateEmbed ctx.User "GLua" desc EmbedColor.Good                
+                | None -> MessageSender.CreateEmbed ctx.User "GLua" "Couln't find this function" EmbedColor.Warning
+            
+            do! ctx.RespondAsync("",false,embed) |> Async.AwaitTask |> Async.Ignore
         } |> Async.StartAsTask :> Task
--- Flamerize/MessageSender.fs
+++ Flamerize/MessageSender.fs
@@ -2,15 +2,17 @@
 
 open DSharpPlus.Entities
 
+type public EmbedColor = Error = 0xe24444 | Warning = 0xe27b44 | Normal = 0xc8c8c8 | Good = 0xdddddd
+
 [<AbstractClass; Sealed>]
 type public MessageSender() =
     
-    static member CreateEmbed (user: DiscordUser) (head: string) (txt: string): DiscordEmbed = 
+    static member CreateEmbed (user: DiscordUser) (head: string) (txt: string) (col: EmbedColor): DiscordEmbed = 
         let builder = new DiscordEmbedBuilder()
         builder.Author <- new DiscordEmbedBuilder.EmbedAuthor()
         builder.Author.Name <- user.Username
         builder.Author.IconUrl <- user.AvatarUrl
-        builder.Color <- new DiscordColor(0xdddddd)
+        builder.Color <- new DiscordColor(LanguagePrimitives.EnumToValue col)
         builder.Footer <- new DiscordEmbedBuilder.EmbedFooter()
         builder.Footer.Text <- head
         builder.Description <- txt
