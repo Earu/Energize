ce600019b8f90d783a7ff875d11e4caf75d57090
Earu <reno.reboursier@gmail.com>
2018-05-09 23:15:35 +0200

    Move unrelated/deprecated stuff
    - Move away Flamerize dir
    - Delete EnergizeMarkov dir
    there wasnt that much interest in making a rust library for this in the end
    - Update the readme

--- Flamerize/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿open Flamerize.Initializer
-open System.Threading.Tasks
-
-[<EntryPoint>]
-let Main _ =
-    Discord.ConnectAsync() |> Async.AwaitTask |> Async.RunSynchronously
-    Task.Delay -1 |> Async.AwaitTask |> Async.RunSynchronously
-    0
