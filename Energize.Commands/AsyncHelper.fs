a882aab856ac3286e1d5c92e2cfbdcc76a849563
Earu <earu.derg@gmail.com>
2019-02-17 12:00:34 +0100

    async helper + first translated command

---
+++ Energize.Commands/AsyncHelper.fs
@@ -0,0 +1,16 @@
+﻿namespace Energize.Commands
+
+module AsyncHelper =
+    open System.Threading.Tasks
+
+    let toTask<'t> (asyncOp : Async<'t>) : Task<'t> =
+        asyncOp |> Async.StartAsTask
+
+    let awaitResult<'t> (task : Task<'t>) : 't =
+        task |> Async.AwaitTask |> Async.RunSynchronously
+
+    let await (task : Task) : unit =
+        task |> Async.AwaitTask |> Async.RunSynchronously
+    
+    let awaitOp<'t> (asyncOp : Async<'t>) : 't =
+       awaitResult (toTask asyncOp)
---
+++ Energize.Commands/Implementation/Util.fs
@@ -0,0 +1,27 @@
+﻿namespace Energize.Commands.Implementation
+
+module Util =
+    open Energize.Commands.CommandHandler
+    open Energize.Commands.AsyncHelper
+
+    let private moduleName = "Utils"
+
+    let private ping : Command = 
+        {
+            name = "ping"
+            callback = 
+                (fun ctx -> async {
+                    let timestamp = ctx.message.Timestamp
+                    let diff = timestamp.Millisecond / 10
+                    awaitResult 
+                        (ctx.messageSender.Good(ctx.message, "Pong!", sprintf ":alarm_clock: Discord: %dms\n:clock1: Bot: %dms" diff ctx.client.Latency))
+                        |> ignore
+                })
+            isLoaded = true
+            usage = "ping <nothing>"
+            help = "Pings the bot"
+            moduleName = moduleName
+        }
+
+    registerCmd ping
+  
