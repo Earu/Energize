685c5dadf7135832f383a93d90eadb45e7b11264
Earu <no.mail@nomail.no>
2019-02-25 17:01:31 +0100

    Finish porting modules

--- Energize.Commands/Implementation/Administration.fs
+++ Energize.Commands/Implementation/Administration.fs
@@ -12,6 +12,9 @@ module Administration =
     open Discord
     open System.Collections.Generic
     open Energize.Toolkit
+    open Energize.Interfaces.Services
+    open Energize.Commands
+    open Discord.Rest
 
     [<AdminOnlyCommand>]
     [<GuildOnlyCommand>]
@@ -43,10 +46,10 @@ module Administration =
             ctx.sendWarn None "Could not find any user for your input"
     }
 
-    let clearCmdBase (ctx : CommandContext) (predicate : IMessage -> bool) =
+    let clearCmdBase (ctx : CommandContext) (amountInput : string) (predicate : IMessage -> bool) =
         let amount =
             let out = ref 0
-            if Int32.TryParse(ctx.arguments.[0], out) then
+            if Int32.TryParse(amountInput, out) then
                 Math.Clamp(out.Value, 1, 100)
             else
                 10
@@ -64,9 +67,9 @@ module Administration =
 
     [<AdminOnlyCommand>]
     [<GuildOnlyCommand>]
-    [<Command("clear", "Clear the bot messages and commands", "clear <amounttoremove|nothing>")>]
+    [<Command("clear", "Clear the bot messages", "clear <amounttoremove|nothing>")>]
     let clear (ctx : CommandContext) = async {
-        clearCmdBase ctx (fun msg -> 
+        clearCmdBase ctx ctx.arguments.[0] (fun msg -> 
             let diff = (DateTime.Now.Date.Ticks - msg.CreatedAt.Date.Ticks)
             let old = diff > (DateTime()).AddDays(15.0).Ticks
             if not old then
@@ -76,3 +79,119 @@ module Administration =
         )
     }
 
+    [<AdminOnlyCommand>]
+    [<GuildOnlyCommand>]
+    [<Command("clearbots", "Clear bot messages", "clearbots <amounttoremove|nothing>")>]
+    let clearBots (ctx : CommandContext) = async {
+        clearCmdBase ctx ctx.arguments.[0] (fun msg -> msg.Author.IsBot)
+    }
+
+    [<AdminOnlyCommand>]
+    [<GuildOnlyCommand>]
+    [<CommandParameters(1)>]
+    [<Command("clearuser", "Clear a user messages", "clearuser <user|userid>,<amounttoremove|nothing>")>]
+    let clearUser (ctx : CommandContext) = async {
+        match findUser ctx ctx.arguments.[0] true with
+        | Some user ->
+            clearCmdBase ctx ctx.arguments.[1] (fun msg -> msg.Author.Id.Equals(user.Id))
+        | None ->   
+            ctx.sendWarn None "No user could be found for your input"
+    }
+
+    [<AdminOnlyCommand>]
+    [<GuildOnlyCommand>]
+    [<Command("clearaw", "Clear a specified amount of messages", "clearaw <amounttoremove|nothing>")>]
+    let clearRaw (ctx : CommandContext) = async {
+        clearCmdBase ctx ctx.arguments.[0] (fun _ -> true)
+    }
+
+    [<AdminOnlyCommand>]
+    [<GuildOnlyCommand>]
+    [<Command("delinvs", "Deletes messages containing discord invites (toggleable)", "delinvs <nothing>")>]
+    let delInvs (ctx : CommandContext) = async {
+        let guser = ctx.message.Author :?> SocketGuildUser
+        let db = ctx.serviceManager.GetService<IDatabaseService>("Database")
+        let dbctx = awaitResult (db.GetContext())
+        let dbguild = awaitResult (dbctx.Instance.GetOrCreateGuild(guser.Guild.Id))
+        dbguild.ShouldDeleteInvites <- not dbguild.ShouldDeleteInvites
+        if dbguild.ShouldDeleteInvites then
+            ctx.sendOK None "Invite links will now be automatically deleted"
+        else
+            ctx.sendOK None "Invite links will now be permitted"
+        dbctx.Dispose()
+    }
+
+    let createHOFChannel (ctx : CommandContext) = 
+        let name = "⭐hall-of-fames"
+        let desc = sprintf "Where %s will post unique messages" (ctx.client.CurrentUser.ToString())
+        let guser = ctx.message.Author :?> SocketGuildUser
+        let created = awaitResult (guser.Guild.CreateTextChannelAsync(name))
+        let everyoneperms = OverwritePermissions(mentionEveryone = PermValue.Deny, sendMessages = PermValue.Deny, sendTTSMessages = PermValue.Deny)
+        let botperms = OverwritePermissions(sendMessages = PermValue.Allow, addReactions = PermValue.Allow)
+        await (created.AddPermissionOverwriteAsync(guser.Guild.EveryoneRole, everyoneperms))
+        await (created.AddPermissionOverwriteAsync(ctx.client.CurrentUser, botperms))
+        await (created.ModifyAsync(Action<TextChannelProperties>(fun prop -> prop.Topic <- Optional(desc))))
+        created :> ITextChannel
+
+    [<AdminOnlyCommand>]
+    [<GuildOnlyCommand>]
+    [<CommandParameters(1)>]
+    [<Command("fame", "Adds a message to the hall of fames", "fame <messageid>")>]
+    let fame (ctx : CommandContext) = async {
+        let db = ctx.serviceManager.GetService<IDatabaseService>("Database")
+        let dbctx = awaitResult (db.GetContext())
+        let guild = (ctx.message.Author :?> SocketGuildUser).Guild :> IGuild
+        let dbguild = awaitResult (dbctx.Instance.GetOrCreateGuild(guild.Id))
+        let chan = 
+            if dbguild.HasHallOfShames then
+                Some ((awaitResult (guild.GetChannelAsync(dbguild.HallOfShameID))) :?> ITextChannel)
+            else
+                try
+
+                    let c = createHOFChannel ctx
+                    dbguild.HallOfShameID <- c.Id
+                    dbguild.HasHallOfShames <- true
+                    Some c
+                with _ ->
+                    None
+        dbctx.Dispose()
+        let msgId = ref 0UL
+        if UInt64.TryParse(ctx.arguments.[0], msgId) && chan.IsSome then
+            let msg = awaitResult (ctx.message.Channel.GetMessageAsync(msgId.Value))
+            match msg with
+            | null ->
+                ctx.sendWarn None "Could not find any message for your input"
+            | m ->
+                let builder = EmbedBuilder()
+                builder
+                    .WithAuthor(msg.Author)
+                    .WithDescription(msg.Content)
+                    .WithFooter("#" + msg.Channel.Name)
+                    .WithTimestamp(msg.CreatedAt)
+                    .WithColor(ctx.messageSender.ColorNormal)
+                    |> ignore
+                match ImageUrlProvider.getLastImgUrl m with
+                | Some url ->
+                    builder.WithImageUrl(url) |> ignore
+                | None -> ()
+
+                let posted = 
+                    match chan.Value with
+                    | :? SocketChannel as chan ->
+                        Some (awaitResult (ctx.messageSender.Send(chan, builder.Build())))
+                    | :? RestChannel as chan ->
+                        Some (awaitResult (ctx.messageSender.Send(chan, builder.Build())))
+                    | _ -> None
+
+                match posted with
+                | None ->
+                    ctx.sendWarn None "There was an error when posting the message into hall of fames"
+                | Some p when not (p.Equals(null)) ->
+                    try
+                        await (p.AddReactionAsync(Emoji("🌟")))
+                    with _ -> ()
+                    ctx.sendOK None "Message successfully added to hall of fames"
+                | _ -> ()
+        else
+            ctx.sendWarn None "This command is expecting a number (message id)"
+    }
---
+++ Energize.Commands/Implementation/Image.fs
@@ -0,0 +1,65 @@
+﻿namespace Energize.Commands.Implementation
+
+open Energize.Commands.Command
+
+[<CommandModule("Image")>]
+module Image =
+    open Energize.Commands.Context
+    open Energize.Commands.UserHelper
+    open Discord
+    open Energize.Commands.AsyncHelper
+    open Discord.WebSocket
+    
+    [<CommandParameters(1)>]
+    [<Command("avatar", "Gets the avatar of a user", "avatar <user|userid>")>]
+    let avatar (ctx : CommandContext) = async {
+        match findUser ctx ctx.arguments.[0] true with
+        | Some user ->
+            let avurl = 
+                let url = user.GetAvatarUrl(ImageFormat.Auto)
+                url.Remove(url.Length - 9)
+            let builder = EmbedBuilder()
+            ctx.messageSender.BuilderWithAuthor(ctx.message, builder)
+            builder
+                .WithFooter(ctx.commandName)
+                .WithImageUrl(avurl)
+                .WithColor(ctx.messageSender.ColorGood)
+                |> ignore
+            awaitIgnore (ctx.messageSender.Send(ctx.message, builder.Build()))
+        | None ->
+            ctx.sendWarn None "Could not find any user for your input"
+    }
+
+    [<GuildOnlyCommand>]
+    [<Command("icon", "Gets the avatar of the guild", "icon <nothing>")>]
+    let icon (ctx : CommandContext) = async {
+        let guser = ctx.message.Author :?> SocketGuildUser
+        let guild = guser.Guild
+        let builder = EmbedBuilder()
+        ctx.messageSender.BuilderWithAuthor(ctx.message, builder)
+        builder
+            .WithFooter(ctx.commandName)
+            .WithImageUrl(guild.IconUrl)
+            .WithColor(ctx.messageSender.ColorGood)
+            |> ignore
+        awaitIgnore (ctx.messageSender.Send(ctx.message, builder.Build()))
+    }
+
+    [<CommandParameters(1)>]
+    [<Command("e", "Gets the picture of a guild emoji","e <guildemoji>")>]
+    let emoji (ctx : CommandContext) = async {
+        let e = ref null
+        if Emote.TryParse(ctx.arguments.[0], e) then
+            let builder = EmbedBuilder()
+            ctx.messageSender.BuilderWithAuthor(ctx.message, builder)
+            builder
+                .WithFooter(ctx.commandName)
+                .WithImageUrl(e.Value.Url)
+                .WithColor(ctx.messageSender.ColorGood)
+                |> ignore
+            awaitIgnore (ctx.messageSender.Send(ctx.message, builder.Build()))
+        else
+            ctx.sendWarn None "A guild emoji is expected as parameter"
+    }
+
+    //rework old image commands
