f2364986c26d00c49229939ddd15d527618e883d
Earu <earu.derg@gmail.com>
2019-05-25 20:19:03 +0200

    Fix command documentation generation

---
+++ Energize.Web/Services/TransmissionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Octovisor.Client;
+using Octovisor.Messages;
+
+namespace Energize.Web.Services
+{
+    public class TransmissionService
+    {
+        public static TransmissionService Instance { get; } = Initialize();
+
+        private static TransmissionService Initialize()
+        {
+            TransmissionService service = new TransmissionService();
+            OctoConfig config = OctoConfig.FromFile("octo_config.yaml");
+            service.Client = new OctoClient(config);
+            service.Client.Log += OnLog;
+
+            return service;
+        }
+
+        static void OnLog(LogMessage log)
+        {
+            if (log.Severity == LogSeverity.Info)
+                Console.WriteLine(log.Content);
+        }
+
+
+        public OctoClient Client { get; private set; }
+    }
+}
