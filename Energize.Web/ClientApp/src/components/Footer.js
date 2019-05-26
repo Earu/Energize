7956fb6d6b2b3268db9cfd413578173ce6d42f86
Earu <earu.derg@gmail.com>
2019-05-27 00:30:43 +0200

    add footer

---
+++ Energize.Web/ClientApp/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Footer extends React.Component
+{
+    displayName = Footer.name;
+
+    render()
+    {
+        return (
+            <footer>
+                Copyright Energize⚡ 2019
+            </footer>
+        );
+    }
+}
