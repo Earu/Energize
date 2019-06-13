808ebfc35c90d958eb42e8fb24f7ddc16be8a941
Earu <earu.derg@gmail.com>
2019-06-13 02:07:35 +0200

    add radio sources

--- Energize.Commands/Implementation/Music.fs
+++ Energize.Commands/Implementation/Music.fs
@@ -326,6 +326,17 @@ module Voice =
     let private radios = [
         ("anime", "https://listen.moe/opus")
         ("kpop", "https://listen.moe/kpop/opus")
+        ("drum n bass", "http://bassdrive.radioca.st/;stream/1")
+        ("metal", "http://ice1.somafm.com/metal-128-mp3")
+        ("dubstep", "http://ice1.somafm.com/dubstep-128-mp3")
+        ("70s", "http://ice1.somafm.com/seventies-128-mp3")
+        ("alt rock", "http://ice1.somafm.com/bagel-128-mp3")
+        ("jazz", "http://ice1.somafm.com/sonicuniverse-128-mp3")
+        ("defcon", "http://ice1.somafm.com/defcon-128-mp3")
+        ("progressive house", "http://ice1.somafm.com/thetrip-128-mp3")
+        ("folk", "http://ice1.somafm.com/folkfwd-128-mp3")
+        ("celtic", "http://ice1.somafm.com/thistle-128-mp3")
+        ("deep house", "http://ice1.somafm.com/beatblender-128-mp3")
     ]
 
     [<CommandParameters(1)>]
