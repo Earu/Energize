aab19a3a092791d6b20948a0d600d540e45fb44f
TheMulti0 <multi@codeprecise.com>
2019-06-23 07:59:54 +0300

    Light Documentation (and renamed IAsyncLavaTrack to IAsyncTrack)

--- Energize.Essentials/TrackTypes/SpotifyTrack.cs
+++ Energize.Essentials/TrackTypes/SpotifyTrack.cs
@@ -5,7 +5,13 @@ using Victoria.Entities;
 
 namespace Energize.Essentials.TrackTypes
 {
-    public class SpotifyTrack : IAsyncLazyLoadTrack, IAsyncLavaTrack
+    /// <summary>
+    /// An AsyncLazyLoadTrack that uses SpotifyTrackInfo for the required metadata instead of the ones of a searched LavaTrack (it does not a LavaTrack until play time)
+    /// At the moment this track does partially depend on a searched LavaTrack for metadata, but this will improve in the future
+    /// <see cref="IAsyncLazyLoadTrack" />
+    /// <see cref="SpotifyTrackInfo" />
+    /// </summary>
+    public class SpotifyTrack : IAsyncLazyLoadTrack
     {
         private LavaTrack _innerTrack;
         private readonly Func<Task<LavaTrack>> _innerTrackCallback;
@@ -24,13 +30,22 @@ namespace Energize.Essentials.TrackTypes
         public async Task<Uri> GetUriAsync() => SpotifyInfo.Uri ?? (await GetInnerTrackAsync()).Uri;
         public Task<string> GetProviderAsync() => Task.FromResult("spotify");
 
-
+        /// <summary>
+        /// Initialize SpotifyTrack with an already searched LavaTrack
+        /// </summary>
+        /// <param name="spotifyInfo">Spotify information from the Spotify API</param>
+        /// <param name="innerTrack">Searched LavaTrack</param>
         public SpotifyTrack(SpotifyTrackInfo spotifyInfo, LavaTrack innerTrack)
         {
             SpotifyInfo = spotifyInfo;
             _innerTrack = innerTrack;
         }
 
+        /// <summary>
+        /// Initialize SpotifyTrack with a callback that searches a LavaTrack (and returns it), for when it is needed
+        /// </summary>
+        /// <param name="spotifyInfo">Spotify information from the Spotify API</param>
+        /// <param name="innerTrackCallback">Method reference to search for the LavaTrack</param>
         public SpotifyTrack(SpotifyTrackInfo spotifyInfo, Func<Task<LavaTrack>> innerTrackCallback)
         {
             SpotifyInfo = spotifyInfo;
