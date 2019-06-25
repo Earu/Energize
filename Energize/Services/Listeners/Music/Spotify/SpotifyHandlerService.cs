bfa4dbb28208445f59b7b29cc3bae5cc779c367b
TheMulti0 <multi@codeprecise.com>
2019-06-25 10:43:11 +0300

    Moved Spotify methods to a different service

--- Energize/Services/Listeners/Music/Spotify/SpotifyHandlerService.cs
+++ Energize/Services/Listeners/Music/Spotify/SpotifyHandlerService.cs
@@ -1,5 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Energize.Essentials;
 using Energize.Essentials.TrackTypes;
 using Energize.Interfaces.Services.Listeners;
 using SpotifyAPI.Web;
@@ -8,31 +14,91 @@ using Victoria;
 
 namespace Energize.Services.Listeners.Music.Spotify
 {
-    public class SpotifyHandlerService : ISpotifyHandlerService
+    [Service("Spotify")]
+    public class SpotifyHandlerService : ServiceImplementationBase, ISpotifyHandlerService
     {
+        private readonly Logger _logger;
         private readonly SpotifyWebAPI _api;
         private readonly LavaRestClient _lavaRest;
         private readonly bool _lazyLoad;
-        
+
+        private readonly Timer _spotifyAuthTimer;
+        private readonly SpotifyTrackProvider _trackProvider;
         private readonly SpotifySearchProvider _searchProvider;
 
-        public SpotifyHandlerService(
-            SpotifyWebAPI api,
-            LavaRestClient lavaRest,
-            bool lazyLoad)
+
+        public SpotifyHandlerService(EnergizeClient client)
         {
-            _api = api;
-            _lavaRest = lavaRest;
-            _lazyLoad = lazyLoad;
-            
+            _logger = client.Logger;
+            _api = new SpotifyWebAPI
+            {
+                TokenType = "Bearer",
+                UseAuth = true,
+                UseAutoRetry = true
+            };
+            _lavaRest = GetLavaRestClient();
+            // TODO: add configuration entry
+            _lazyLoad = true;
+            _spotifyAuthTimer = new Timer(TradeSpotifyToken);
+
+            _trackProvider = new SpotifyTrackProvider(_api, _lavaRest, _lazyLoad);
             _searchProvider = new SpotifySearchProvider(_api, _lavaRest, _lazyLoad);
         }
+
+        private static LavaRestClient GetLavaRestClient()
+        {
+            var config = new Configuration
+            {
+                ReconnectInterval = TimeSpan.FromSeconds(15),
+                ReconnectAttempts = 3,
+                Host = Config.Instance.Lavalink.Host,
+                Port = Config.Instance.Lavalink.Port,
+                Password = Config.Instance.Lavalink.Password,
+                SelfDeaf = false,
+                BufferSize = 8192,
+                PreservePlayers = true,
+                AutoDisconnect = false,
+                LogSeverity = LogSeverity.Debug,
+                DefaultVolume = 50,
+                InactivityTimeout = TimeSpan.FromMinutes(3)
+            };
+
+            return new LavaRestClient(config);
+        }
+
+        private async void TradeSpotifyToken(object _)
+        {
+            void Callback(HttpWebRequest req)
+            {
+                byte[] credBytes = Encoding.UTF8.GetBytes($"{Config.Instance.Spotify.ClientID}:{Config.Instance.Spotify.ClientSecret}");
+                req.Headers[HttpRequestHeader.Authorization] = $"Basic {Convert.ToBase64String(credBytes)}";
+                req.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            }
+
+            string json = await HttpClient.PostAsync(
+                "https://accounts.spotify.com/api/token?grant_type=client_credentials",
+                string.Empty,
+                _logger,
+                null,
+                Callback);
+
+            Dictionary<string, string> keys = JsonPayload.Deserialize<Dictionary<string, string>>(json, _logger);
+            if (keys.ContainsKey("access_token"))
+            {
+                _api.AccessToken = keys["access_token"];
+            }
+        }
+
+        public override Task InitializeAsync()
+        {
+            _spotifyAuthTimer.Change(0, 3600 * 1000);
+            return Task.CompletedTask;
+        }
+
+        public Task<SpotifyTrack> GetTrackAsync(string id) 
+            => _trackProvider.GetTrackAsync(id);
+
         public Task<IEnumerable<SpotifyTrack>> SearchAsync(string query, SearchType searchType = SearchType.All) 
             => _searchProvider.SearchAsync(query, searchType);
-
-        public Task<SpotifyTrack> GetTrackAsync(string id)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
