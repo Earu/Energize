d52da41a0e7eca9f707ce6b82297652fafa4db2b
Earu <earu.derg@gmail.com>
2019-07-31 00:58:55 +0200

    Refactor transmission service into transmitters

---
+++ Energize/Services/Transmission/Transmitters/WatchdogTransmitter.cs
@@ -0,0 +1,28 @@
+﻿using Energize.Interfaces.Services.Listeners;
+using Octovisor.Client;
+using System;
+
+namespace Energize.Services.Transmission.Transmitters
+{
+    internal class WatchdogTransmitter : BaseTransmitter
+    {
+        private readonly ServiceManager ServiceManager;
+
+        internal WatchdogTransmitter(EnergizeClient client, OctoClient octoClient) : base(client, octoClient)
+        {
+            this.ServiceManager = client.ServiceManager;
+        }
+
+        internal override void Initialize()
+            => this.Client.OnTransmission<string>("reconnect", this.OnReconnectRequested);
+
+        private async void OnReconnectRequested(RemoteProcess proc, string publicIp)
+        {
+            if (string.IsNullOrWhiteSpace(publicIp) || proc.Name != "Watchdog") return;
+            this.Logger.Nice("IPC", ConsoleColor.Yellow, $"Remote nodes re-connected with IP: {publicIp}");
+            IMusicPlayerService music = this.ServiceManager.GetService<IMusicPlayerService>("Music");
+            await music.DisconnectAllPlayersAsync("Network issues detected, disconnecting to prevent further bugs");
+            await music.StartAsync(publicIp);
+        }
+    }
+}
---
+++ Energize/Services/Transmission/Transmitters/WebsiteTransmitter.cs
@@ -0,0 +1,50 @@
+﻿using Discord.WebSocket;
+using Energize.Services.Listeners;
+using Energize.Services.Transmission.TransmissionModels;
+using Octovisor.Client;
+using System;
+using System.Linq;
+
+namespace Energize.Services.Transmission.Transmitters
+{
+    internal class WebsiteTransmitter : BaseTransmitter
+    {
+        private readonly ServiceManager ServiceManager;
+        private readonly DiscordShardedClient DiscordClient;
+        private readonly string Prefix;
+
+        internal WebsiteTransmitter(EnergizeClient client, OctoClient octoClient) : base(client, octoClient)
+        {
+            this.ServiceManager = client.ServiceManager;
+            this.DiscordClient = client.DiscordClient;
+        }
+
+        internal override void Initialize()
+        {
+            this.Client.OnTransmission<object, CommandInformation>("commands", this.OnCommandsRequested);
+            this.Client.OnTransmission<object, BotInformation>("info", this.OnInformationRequested);
+        }
+
+        private CommandInformation OnCommandsRequested(RemoteProcess proc, object _)
+        {
+            CommandHandlingService commandService = this.ServiceManager.GetService<CommandHandlingService>("Commands");
+            this.Logger.Nice("IPC", ConsoleColor.Magenta, $"Sent command information to process \'{proc}\'");
+            return new CommandInformation
+            {
+                BotMention = this.DiscordClient.CurrentUser.ToString(),
+                Prefix = this.Prefix,
+                Commands = commandService.RegisteredCommands.ToList().Select(kv => Command.ToModel(kv.Value)).ToList()
+            };
+        }
+
+        private BotInformation OnInformationRequested(RemoteProcess proc, object _)
+        {
+            this.Logger.Nice("IPC", ConsoleColor.Magenta, $"Sent bot information to process \'{proc}\'");
+            return new BotInformation
+            {
+                ServerCount = this.DiscordClient.Guilds.Count,
+                UserCount = this.DiscordClient.Guilds.Sum(guild => guild.Users.Count),
+            };
+        }
+    }
+}
