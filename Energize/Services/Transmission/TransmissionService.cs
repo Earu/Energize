ee85abd9093f691109539d45aa3963f15412217d
Earu <earu.derg@gmail.com>
2019-07-31 00:58:55 +0200

    Refactor transmission service into transmitters

--- Energize/Services/Transmission/TransmissionService.cs
+++ Energize/Services/Transmission/TransmissionService.cs
@@ -1,13 +1,10 @@
-﻿using Discord.WebSocket;
-using Energize.Essentials;
-using Energize.Services.Listeners;
-using Energize.Services.Transmission.TransmissionModels;
+﻿using Energize.Essentials;
+using Energize.Services.Transmission.Transmitters;
 using Octovisor.Client;
 using Octovisor.Messages;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Energize.Interfaces.Services.Listeners;
 
 namespace Energize.Services.Transmission
 {
@@ -15,16 +12,11 @@ namespace Energize.Services.Transmission
     public class TransmissionService : ServiceImplementationBase
     {
         private readonly OctoClient OctoClient;
-        private readonly string Prefix;
-        private readonly DiscordShardedClient DiscordClient;
         private readonly Logger Logger;
-        private readonly ServiceManager ServiceManager;
+        private readonly List<BaseTransmitter> Transmitters;
 
         public TransmissionService(EnergizeClient client)
         {
-            this.Prefix = client.Prefix;
-            this.DiscordClient = client.DiscordClient;
-            this.ServiceManager = client.ServiceManager;
             OctovisorConfig config = Config.Instance.Octovisor;
             OctoConfig octoConfig = new OctoConfig
             {
@@ -41,6 +33,12 @@ namespace Energize.Services.Transmission
                 if (log.Severity == LogSeverity.Info)
                     this.Logger.Nice("Octovisor", ConsoleColor.Magenta, log.Content);
             };
+
+            this.Transmitters = new List<BaseTransmitter>
+            {
+                new WebsiteTransmitter(client, this.OctoClient),
+                new WatchdogTransmitter(client, this.OctoClient)
+            };
         }
 
         public override async Task InitializeAsync()
@@ -48,37 +46,7 @@ namespace Energize.Services.Transmission
             try
             {
                 await this.OctoClient.ConnectAsync();
-
-                this.OctoClient.OnTransmission<object, CommandInformation>("commands", (proc, _) =>
-                {
-                    CommandHandlingService commandService = this.ServiceManager.GetService<CommandHandlingService>("Commands");
-                    this.Logger.Nice("IPC", ConsoleColor.Magenta, $"Sent command information to process \'{proc}\'");
-                    return new CommandInformation
-                    {
-                        BotMention = this.DiscordClient.CurrentUser.ToString(),
-                        Prefix = this.Prefix,
-                        Commands = commandService.RegisteredCommands.ToList().Select(kv => Command.ToModel(kv.Value)).ToList()
-                    };
-                });
-
-                this.OctoClient.OnTransmission<object, BotInformation>("info", (proc, _) =>
-                {
-                    this.Logger.Nice("IPC", ConsoleColor.Magenta, $"Sent bot information to process \'{proc}\'");
-                    return new BotInformation
-                    {
-                        ServerCount = this.DiscordClient.Guilds.Count,
-                        UserCount = this.DiscordClient.Guilds.Sum(guild => guild.Users.Count),
-                    };
-                });
-
-                this.OctoClient.OnTransmission<string>("reconnect", (proc, publicIp) =>
-                {
-                    if (string.IsNullOrWhiteSpace(publicIp) || proc.Name != "Watchdog") return;
-                    this.Logger.Nice("IPC", ConsoleColor.Yellow, $"Remote nodes re-connected with IP: {publicIp}");
-                    IMusicPlayerService music = this.ServiceManager.GetService<IMusicPlayerService>("Music");
-                    music.DisconnectAllPlayersAsync("Network issues detected, disconnecting to prevent further bugs");
-                    music.StartAsync(publicIp);
-                });
+                this.Transmitters.ForEach(transmitter => transmitter.Initialize());
             }
             catch (Exception ex)
             {
---
+++ Energize/Services/Transmission/Transmitters/BaseTransmitter.cs
@@ -0,0 +1,19 @@
+﻿using Energize.Essentials;
+using Octovisor.Client;
+
+namespace Energize.Services.Transmission.Transmitters
+{
+    internal class BaseTransmitter
+    {
+        protected OctoClient Client;
+        protected Logger Logger;
+
+        internal BaseTransmitter(EnergizeClient client, OctoClient octoClient)
+        {
+            this.Client = octoClient;
+            this.Logger = client.Logger;
+        }
+
+        internal virtual void Initialize() { }
+    }
+}
