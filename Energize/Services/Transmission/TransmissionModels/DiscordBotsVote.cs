eb73b6ae8b389ea0608baba84402c69b3e1856e1
Earu <earu.derg@gmail.com>
2019-07-31 18:00:34 +0200

    Implement update and upvote handlers

---
+++ Energize/Services/Transmission/TransmissionModels/DiscordBotsVote.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Energize.Services.Transmission.TransmissionModels
+{
+    public class DiscordBotsVote
+    {
+        [JsonProperty("bot")]
+        public ulong BotId;
+
+        [JsonProperty("user")]
+        public ulong UserId;
+
+        [JsonProperty("isWeekend")]
+        public bool IsWeekend;
+    }
+}
